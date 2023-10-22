fd32b58d08ef35cee65bf325525b79039a259d70
Kalen White <kalen@krworks.com>
2023-10-22 09:39:33 -0700

    work on textmate loading

--- textmate/apps/textmate-test.cpp
+++ textmate/apps/textmate-test.cpp
@@ -1,15 +1,20 @@
-#include <ux/ux.hpp>
+//#include <ux/ux.hpp>
+#include <textmate/textmate.hpp>
 
 using namespace ion;
 
 int main(int argc, char *argv[]) {
 	RegEx regex(utf16("\\w+"), RegEx::Behaviour::global);
-    array<utf16> matches = regex.exec(utf16("Hello, World!"));
+    array<indexed<utf16>> matches = regex.exec(utf16("Hello, World!"));
 
 	if (matches)
 		console.log("match: {0}", {matches[0]});
 
-	var grammar = path("style/js.json").read<var>();
+	//var grammar = path("style/js.json").read<var>();
+	//int test = 0;
+	//test++;
+
+	oni_test("style/js.json");
 
     ///
     return 0;
--- textmate/textmate.cpp
+++ textmate/textmate.cpp
@@ -392,6 +392,11 @@ struct RawGrammar:mx {
 	mx_basic(RawGrammar);
 
 	RawGrammar(null_t) : RawGrammar() { }
+
+	RawGrammar(var raw) : RawGrammar() {
+		int test = 0;
+		test++;
+	}
 	
 	operator bool() {
 		return data->name;
@@ -4733,8 +4738,8 @@ struct Registry:mx {
 			return _doLoadSingleGrammar(scopeName);
 		}
 
-		Grammar _doLoadSingleGrammar(ScopeName scopeName) {
-			Grammar grammar = _options.loadGrammar(scopeName);
+		RawGrammar _doLoadSingleGrammar(ScopeName scopeName) {
+			RawGrammar grammar = _options.loadGrammar(scopeName);
 			lambda<array<ScopeName>(ScopeName)> injections =
 				_options.getInjections.type() == typeof(lambda<array<ScopeName>(ScopeName)>) ?
 					_options.getInjections(scopeName) : array<ScopeName> {};
@@ -4782,6 +4787,63 @@ struct Registry:mx {
 		data->_syncRegistry = SyncRegistry(Theme::members::createFromRawTheme(options.theme, options.colorMap));
 	}
 };
+
+void oni_test(path tm) {
+	RawGrammar grammar = tm.read<RawGrammar>();
+	
+	// Load the JavaScript grammar and any other grammars included by it async.
+	const text = [
+		`function sayHello(name) {`,
+		`\treturn "Hello, " + name;`,
+		`}`
+	];
+	let ruleStack = vsctm.INITIAL;
+	for (let i = 0; i < text.length; i++) {
+		const line = text[i];
+		const lineTokens = grammar.tokenizeLine(line, ruleStack);
+		console.log(`\nTokenizing line: ${line}`);
+		for (let j = 0; j < lineTokens.tokens.length; j++) {
+			const token = lineTokens.tokens[j];
+			console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
+			`(${line.substring(token.startIndex, token.endIndex)}) ` +
+			`with scopes ${token.scopes.join(', ')}`
+			);
+		}
+		ruleStack = lineTokens.ruleStack;
+	}
+
+	/* OUTPUT:
+
+	Unknown scope name: source.js.regexp
+
+	Tokenizing line: function sayHello(name) {
+	- token from 0 to 8 (function) with scopes source.js, meta.function.js, storage.type.function.js
+	- token from 8 to 9 ( ) with scopes source.js, meta.function.js
+	- token from 9 to 17 (sayHello) with scopes source.js, meta.function.js, entity.name.function.js
+	- token from 17 to 18 (() with scopes source.js, meta.function.js, punctuation.definition.parameters.begin.js
+	- token from 18 to 22 (name) with scopes source.js, meta.function.js, variable.parameter.function.js
+	- token from 22 to 23 ()) with scopes source.js, meta.function.js, punctuation.definition.parameters.end.js
+	- token from 23 to 24 ( ) with scopes source.js
+	- token from 24 to 25 ({) with scopes source.js, punctuation.section.scope.begin.js
+
+	Tokenizing line:        return "Hello, " + name;
+	- token from 0 to 1 (  ) with scopes source.js
+	- token from 1 to 7 (return) with scopes source.js, keyword.control.js
+	- token from 7 to 8 ( ) with scopes source.js
+	- token from 8 to 9 (") with scopes source.js, string.quoted.double.js, punctuation.definition.string.begin.js
+	- token from 9 to 16 (Hello, ) with scopes source.js, string.quoted.double.js
+	- token from 16 to 17 (") with scopes source.js, string.quoted.double.js, punctuation.definition.string.end.js
+	- token from 17 to 18 ( ) with scopes source.js
+	- token from 18 to 19 (+) with scopes source.js, keyword.operator.arithmetic.js
+	- token from 19 to 20 ( ) with scopes source.js
+	- token from 20 to 24 (name) with scopes source.js, support.constant.dom.js
+	- token from 24 to 25 (;) with scopes source.js, punctuation.terminator.statement.js
+
+	Tokenizing line: }
+	- token from 0 to 1 (}) with scopes source.js, punctuation.section.scope.end.js
+
+	*/
+}
 
 #if 0
 
