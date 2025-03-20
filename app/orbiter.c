8cb824cfa654d59188553057e174f83b6a7126d0
Kalen Novis White <kalen@krworks.com>
2025-03-20 08:12:43 -0700

    refactor uses of vector / structs

--- app/orbiter.c
+++ app/orbiter.c
@@ -13,11 +13,10 @@ int main(int argc, cstr argv[]) {
     /// orbiter contains data of all structure/fields in glTF file
 
     /// initialize
-    trinity  t         = trinity  ( );
-    window   w         = window   ( trinity, t, title, 
-                                    string("orbiter"), width, 800, height, 800 );
+    trinity  t = trinity  ( );
+    window   w = window   ( t, t, title, string("orbiter"), width, 800, height, 800 );
 
-    shader    pbr            = shader   ( trinity, t, name,  string("pbr"), ray_depth, 4 );
+    shader    pbr            = shader   ( t, t, name,  string("pbr"));
     Node      id_flower      = find     ( orbiter,  "flower");
     Primitive pr_containment = primitive(id_flower, orbiter, "flower1");
     Node      id_body        = find     ( orbiter,  "body");
@@ -26,26 +25,34 @@ int main(int argc, cstr argv[]) {
     Primitive body_outside3  = primitive(id_body, orbiter, "body3");
     Primitive body_outside4  = primitive(id_body, orbiter, "body4");
     array    flower_shaded   = array_of (
-        part(id, pr_containment, shader, pbr), null );
+        part(id, pr_containment, s, pbr), null );
     
     array    body_shaded     = array_of (
-        part(id, body_outside1, shader, pbr), 
-        part(id, body_outside2, shader, pbr), 
-        part(id, body_outside3, shader, pbr), 
-        part(id, body_outside4, shader, pbr), null );
+        part(id, body_outside1, s, pbr), 
+        part(id, body_outside2, s, pbr), 
+        part(id, body_outside3, s, pbr), 
+        part(id, body_outside4, s, pbr), null );
 
     node     flower    = node     ( id, id_flower, parts, flower_shaded );
     node     body      = node     ( id, id_body,   parts, body_shaded );
 
-    image sampler = image(uri, form(path, "images/ion-orbiter.png"), surface, Surface_color);
-    Camera camera = Camera();
+    image sampler     = image(uri, form(path, "images/ion-orbiter.png"),       surface, Surface_color);
+    image environment = image(uri, form(path, "images/qwantani_night_4k.exr"), surface, Surface_environment);
+    World world       = World();
+
+    // give us top level 
+    // give us ALL the method NAMES we need for setting up proj and view
+    mat4f_set_identity(&world->model);
+    mat4f_set_identity(&world->view);
+    mat4f_set_identity(&world->proj);
+
     /// create model, describing the nodes shade
     /// debug sampler creation, uniforms
-    model    agent     = model    ( trinity,  t,
+    model    agent     = model    ( t,        t,
                                     w,        w,
                                     id,       orbiter, 
-                                    samplers, a(sampler, null),
-                                    uniforms, a(camera, null),
+                                    samplers, a(sampler, environment, null),
+                                    uniforms, a(world, null),
                                     nodes,    a(body, flower, null) );
     
     /// push this model to our scene, going on in the window
--- res/shaders/cube.vert
+++ res/shaders/cube.vert
@@ -1,6 +1,11 @@
 #include <import>
 
-layout(location = 0) in  vec3 pos;
+// these come from the vbo as read in from gltf direct
+// we let gltf dictate what our vertex is; we export this direct with controls over that
+layout(location = 0) in vec3 pos;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 uv;
+layout(location = 3) in vec4 tangents;
 
 void main() {
     gl_Position = vec4(pos, 1.0);
